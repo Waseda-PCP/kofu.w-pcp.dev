ea59f9c33458e590786decba4c9eeb6596739b17
yama.can <yama.can95@gmail.com>
2024-09-24 21:31:24 +0900

    [add] Add links and contents

--- src/app/2024/page.tsx
+++ src/app/2024/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image"
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
@@ -23,23 +22,86 @@ export default async function Page() {
 			</div>
 
 			<div className="topcol">
-				<h2>PCPについて</h2>
+				<h2 id="about-pcp">PCPについて</h2>
+
+				<p>
+
+					PCプログラミング部は、早稲田中学校・高等学校の公式部活動です。
+					略称として、W-PCP と呼ばれています。
+					W-PCP では、競技プログラミング、Web開発、ゲーム開発などを行っています。
+
+				</p>
 			</div>
 
 			<div className="topcol">
-				<h2>展示内容</h2>
+				<h2 id="contents">展示内容</h2>
+
+				<p>
+
+					興風祭で、部員が制作したゲームを展示しています。
+					是非お越しください。
+
+				</p>
 			</div>
 
 			<div className="topcol">
-				<h2>アンケート</h2>
+				<h2 id="enquetes">アンケート</h2>
+
+				<p>
+					アンケートは準備中です。しばらくお待ちください。
+				</p>
 			</div>
 
 			<div className="topcol">
-				<h2>ゲーム配布</h2>
+				<h2 id="games">ゲーム配布</h2>
+
+				<p>
+					興風祭 2024 にて W-PCP へお越しいただき、アンケートに答えてくださった方は <a href="/2024/download">ダウンロードページ</a> より、ゲームをダウンロードできます。<br />
+					ぜひ、ダウンロードして遊んでみてください。
+				</p>
 			</div>
 
 			<div className="topcol">
-				<h2>アクセス</h2>
+				<h2 id="access">アクセス</h2>
+
+				<table>
+
+					<tbody>
+
+						<tr>
+
+							<td>開催</td>
+							<td>早稲田中学校・高等学校</td>
+
+						</tr>
+
+						<tr>
+
+							<td>住所</td>
+							<td>〒162-8654 東京都新宿区馬場下町 62番地</td>
+
+						</tr>
+
+						<tr>
+
+							<td>日時</td>
+							<td>2024年 9月28日、9月29日 9:00 ~ 16:00</td>
+
+						</tr>
+
+						<tr>
+
+							<td>展示室</td>
+							<td>1号館（高校棟）3階 高校3年 E組、F組</td>
+
+						</tr>
+
+					</tbody>
+
+				</table>
+
+				<p>更に詳細な情報を確認するには、<a href="https://www.waseda-h.ed.jp/%e3%82%a2%e3%82%af%e3%82%bb%e3%82%b9/"> 早稲田中学校・高等学校の公式サイト</a> をご覧ください。</p>
+
 			</div>
 
 		</div>
