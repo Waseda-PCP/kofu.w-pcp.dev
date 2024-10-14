83402bfd13bba3ec9fb8e4bb12778bd766e3704f
yama.can <yama.can95@gmail.com>
2024-10-14 10:56:48 +0900

    [fix] Fix some styles

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
 export default async function NotFound() {
 
 	return (
-		<div className="notfound">
+		<div className="topcol">
 			<h1>THE NOT FOUND PAGE.</h1>
 			<p>あなたはまだ作られていないページにアクセスしました。リンクを編集したようですね。エンジニアなのでしょうか。</p>
 			<p>もしエンジニアでないのなら、<a href="mailto:support@w-pcp.dev">support@w-pcp.dev</a> への連絡をお待ちしております。</p>
